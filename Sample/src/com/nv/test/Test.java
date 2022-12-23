ca6dbf38daa6a1d2db3ee2314f795ac9ae371938
naveenv1608 <goud1608@mail.com>
2022-12-24 03:45:23 +0530

    Git BASH COMMAND

--- Sample/src/com/nv/test/Test.java
+++ Sample/src/com/nv/test/Test.java
@@ -3,6 +3,6 @@ package com.nv.test;
 public class Test {
   public static void main(String[] args) {
 	System.out.println("Test");
-	System.out.println("Naveen Veeramalla");
+	System.out.println("Naveen  Veeramalla");
 }
 }
